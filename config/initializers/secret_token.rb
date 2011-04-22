d7c8a4173f24e263c9ad0080744ca13b83f1c23a
thirdshell <remo@rmeng.ch>
2011-04-22 18:48:20 +0200

    initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '7e374e90584bdb2792b35c539805d3e39db2ab65dfb02683e9a02677ba2d3c0a23ccf50a1bdff909b761792eddc538f6ab99f0ff0982e92051edb37d7df29621'
